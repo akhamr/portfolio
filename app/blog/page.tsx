25a41071c2b8385d2d96f586c5782b4eb0f975d2
Khamid Arrazaq <saikonomia@gmail.com>
2023-07-24 20:43:36 +0700

    Mamam

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,4 +1,11 @@
-export default function Home() {
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: 'Blog',
+    description: 'Mamam',
+};
+
+export default function Blog() {
     return (
         <div className="flex h-full flex-col items-center justify-center">
             <p className="text-5xl font-bold tracking-tight">Coming Soon!</p>
