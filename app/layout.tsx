957def3f088c555f2cbd94c35001811dfb7e7edc
Khamid Arrazaq <saikonomia@gmail.com>
2023-07-24 21:05:30 +0700

    coming soon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import '@/styles/global.css';
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { Nunito } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] });
+const nunito = Nunito({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
     title: {
@@ -28,8 +28,12 @@ export default function RootLayout({
     children: React.ReactNode;
 }) {
     return (
-        <html lang="en">
-            <body className={inter.className}>{children}</body>
+        <html lang="en" className={nunito.className}>
+            <body className="flex h-screen flex-col justify-between">
+                <div id="main-content" className="mx-auto max-w-[85%] flex-1">
+                    {children}
+                </div>
+            </body>
         </html>
     );
 }
