83d60ba520681ce04d8d1b885f5c84ecd391f046
Khamid Arrazaq <saikonomia@gmail.com>
2023-07-25 14:47:46 +0700

    fix font

--- styles/fonts.tsx
+++ styles/fonts.tsx
@@ -1,15 +1,18 @@
 import { Mansalva, Nunito, Gloria_Hallelujah } from 'next/font/google';
 
 export const doodle = Mansalva({
+    subsets: ['latin'],
     variable: '--font-doodle',
     weight: '400',
 });
 
 export const sans = Nunito({
+    subsets: ['latin'],
     variable: '--font-nunito',
 });
 
 export const footer = Gloria_Hallelujah({
+    subsets: ['latin'],
     variable: '--font-footer',
     weight: '400',
 });
