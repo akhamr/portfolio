3a06130ee6a95647393a8bcc06d47971638b3b51
Khamid Arrazaq <saikonomia@gmail.com>
2023-07-31 13:42:05 +0700

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,6 @@
-import dynamic from 'next/dynamic';
+import { Doodle1, Doodle2 } from '@/components/Doodle';
 import Image from 'next/image';
 import Link from 'next/link';
-
-const Doodle1 = dynamic(() => import('@/components/doodle/Doodle1'), {
-    ssr: false,
-});
-
-const Doodle2 = dynamic(() => import('@/components/doodle/Doodle2'), {
-    ssr: false,
-});
 
 export default function Home() {
     return (
