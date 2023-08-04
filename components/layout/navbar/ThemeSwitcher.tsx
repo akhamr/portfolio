e0cb5c5feb92949380e4296efb72f4540c470715
Khamid Arrazaq <saikonomia@gmail.com>
2023-08-04 10:21:48 +0700

    tes

--- components/layout/navbar/ThemeSwitcher.tsx
+++ components/layout/navbar/ThemeSwitcher.tsx
@@ -1,18 +1,8 @@
 import { useTheme } from 'next-themes';
-import { useState, useEffect } from 'react';
 
 export default function ThemeSwitcher() {
-    const [mounted, setMounted] = useState(false);
     const { theme, setTheme } = useTheme();
     const isDark = theme === 'dark';
-
-    useEffect(() => {
-        setMounted(true);
-    }, []);
-
-    if (!mounted) {
-        return null;
-    }
 
     return (
         <input
