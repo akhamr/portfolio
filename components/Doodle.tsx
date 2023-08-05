0c0cc4db937d0c89369d702d660e1fdfe28b65a9
Khamid Arrazaq <saikonomia@gmail.com>
2023-08-05 10:03:21 +0700

    test

--- components/Doodle.tsx
+++ components/Doodle.tsx
@@ -5,7 +5,7 @@ import { motion } from 'framer-motion';
 
 export function Doodle1() {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="-30 0 300 200">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="-32 0 300 200">
             <motion.path
                 fill="none"
                 strokeWidth={5}
