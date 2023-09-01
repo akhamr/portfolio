43fa6f5c4be6c99ff5748aa395d64eb33c58612b
Khamid Arrazaq <saikonomia@gmail.com>
2023-09-01 23:05:28 +0700

    tes

--- components/spotify/NowPlaying.tsx
+++ components/spotify/NowPlaying.tsx
@@ -27,21 +27,23 @@ export default function NowPlaying({
                 <div className="flex h-[100px] gap-3 rounded-md border-2 border-dashed border-gray-200 p-2 hover:bg-zinc-300 dark:border-gray-800 dark:hover:bg-zinc-700">
                     <Image
                         className="rounded-md"
-                        src={albumImageUrl || NO_COVER}
-                        alt={title || "Not listening to anything"}
+                        src={(isPlaying == true && albumImageUrl) || NO_COVER}
+                        alt={(isPlaying == true && title) || "Not playing"}
                         height={80}
                         width={80}
                         style={{ width: 80, height: 80 }}
                     />
                     <div className="flex flex-col justify-center gap-1 text-[#202020] dark:text-gray-300">
-                        {(
+                        {(isPlaying == true && (
                             <>
                                 <h5 className="m-0 line-clamp-1">{title}</h5>
                                 <p className="m-0 line-clamp-1 text-sm md:text-base">
                                     {artist}
                                 </p>
                             </>
-                        ) || <h5 className="m-0">Not listening to anything</h5>}
+                        )) || (
+                            <h5 className="m-0">Not listening to anything</h5>
+                        )}
                     </div>
                 </div>
             </Link>
