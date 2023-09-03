77ddf6242fdc2235297609e146125bd76a5bee7f
Khamid Arrazaq <saikonomia@gmail.com>
2023-09-03 15:21:13 +0700

    tes

--- app/api/now-playing/route.ts
+++ app/api/now-playing/route.ts
@@ -5,13 +5,33 @@ export async function GET() {
     const res = await getNowPlaying();
 
     if (res.status === 204 || res.status > 400) {
-        return NextResponse.json({ isPlaying: false });
+        return NextResponse.json(
+            { isPlaying: false },
+            {
+                status: 200,
+                headers: {
+                    "content-type": "application/json",
+                    "cache-control":
+                        "public, s-maxage=60, stale-while-revalidate=30",
+                },
+            }
+        );
     }
 
     const song = await res.json();
 
     if (song.item === null) {
-        return NextResponse.json({ isPlaying: false });
+        return NextResponse.json(
+            { isPlaying: false },
+            {
+                status: 200,
+                headers: {
+                    "content-type": "application/json",
+                    "cache-control":
+                        "public, s-maxage=60, stale-while-revalidate=30",
+                },
+            }
+        );
     }
 
     const isPlaying = song.is_playing;
@@ -22,11 +42,21 @@ export async function GET() {
     const albumImageUrl = song.item.album.images[0].url;
     const songUrl = song.item.external_urls.spotify;
 
-    return NextResponse.json({
-        albumImageUrl,
-        artist,
-        isPlaying,
-        songUrl,
-        title,
-    });
+    return NextResponse.json(
+        {
+            albumImageUrl,
+            artist,
+            isPlaying,
+            songUrl,
+            title,
+        },
+        {
+            status: 200,
+            headers: {
+                "content-type": "application/json",
+                "cache-control":
+                    "public, s-maxage=60, stale-while-revalidate=30",
+            },
+        }
+    );
 }
--- components/spotify/NowPlaying.tsx
+++ components/spotify/NowPlaying.tsx
@@ -9,47 +9,43 @@ export default function NowPlaying() {
     const fetcher = (url: string) => fetch(url).then((r) => r.json());
     const { data, isLoading } = useSWR("/api/now-playing", fetcher);
 
-    return (
-        (!isLoading && (
-            <Link
-                href={(data.isPlaying && data.songUrl) || "#playlist"}
-                className="font-normal no-underline"
-            >
-                <div className="flex h-[100px] gap-3 rounded-md border-2 border-dashed border-gray-200 p-2 hover:bg-zinc-300 dark:border-gray-800 dark:hover:bg-zinc-700">
-                    <Image
-                        className="rounded-md"
-                        src={(data.isPlaying && data.albumImageUrl) || NO_COVER}
-                        alt={(data.isPlaying && data.title) || "Not playing"}
-                        height={80}
-                        width={80}
-                        style={{ width: 80, height: 80 }}
-                    />
-                    <div className="flex flex-col justify-center gap-1 text-[#202020] dark:text-gray-300">
-                        {(data.isPlaying && (
-                            <>
-                                <h5 className="m-0 line-clamp-1">
-                                    {data.title}
-                                </h5>
-                                <p className="m-0 line-clamp-1 text-sm md:text-base">
-                                    {data.artist}
-                                </p>
-                            </>
-                        )) || (
-                            <h5 className="m-0">Not listening to anything</h5>
-                        )}
-                    </div>
-                </div>
-            </Link>
-        )) || (
-            <div className="h-[100px] rounded-md border-2 border-dashed border-gray-200 p-2 dark:border-gray-800">
-                <div className="flex animate-pulse items-center gap-3">
-                    <div className="h-[80px] w-[80px] rounded-md bg-zinc-300 dark:bg-zinc-700"></div>
-                    <div className="flex flex-col justify-center gap-3">
-                        <div className="h-6 w-40 rounded bg-zinc-300 dark:bg-zinc-700 md:w-64"></div>
-                        <div className="h-5 w-28 rounded bg-zinc-300 dark:bg-zinc-700 md:w-48"></div>
-                    </div>
+    return !isLoading ? (
+        <Link
+            href={data.isPlaying ? data.songUrl : "#playlist"}
+            className="font-normal no-underline"
+        >
+            <div className="flex h-[100px] gap-3 rounded-md border-2 border-dashed border-gray-200 p-2 hover:bg-zinc-300 dark:border-gray-800 dark:hover:bg-zinc-700">
+                <Image
+                    className="rounded-md"
+                    src={data.isPlaying ? data.albumImageUrl : NO_COVER}
+                    alt={data.isPlaying ? data.title : "Not playing"}
+                    height={80}
+                    width={80}
+                    style={{ width: 80, height: 80 }}
+                />
+                <div className="flex flex-col justify-center gap-1 text-[#202020] dark:text-gray-300">
+                    {data.isPlaying ? (
+                        <>
+                            <h5 className="m-0 line-clamp-1">{data.title}</h5>
+                            <p className="m-0 line-clamp-1 text-sm md:text-base">
+                                {data.artist}
+                            </p>
+                        </>
+                    ) : (
+                        <h5 className="m-0">Not listening to anything</h5>
+                    )}
                 </div>
             </div>
-        )
+        </Link>
+    ) : (
+        <div className="h-[100px] rounded-md border-2 border-dashed border-gray-200 p-2 dark:border-gray-800">
+            <div className="flex animate-pulse items-center gap-3">
+                <div className="h-[80px] w-[80px] rounded-md bg-zinc-300 dark:bg-zinc-700"></div>
+                <div className="flex flex-col justify-center gap-3">
+                    <div className="h-6 w-40 rounded bg-zinc-300 dark:bg-zinc-700 md:w-64"></div>
+                    <div className="h-5 w-28 rounded bg-zinc-300 dark:bg-zinc-700 md:w-48"></div>
+                </div>
+            </div>
+        </div>
     );
 }
