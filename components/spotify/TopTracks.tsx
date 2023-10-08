4b5d320be385b72c03d2afdb410e98a4b83ec108
Khamid Arrazaq <saikonomia@gmail.com>
2023-10-08 18:58:57 +0700

    tes

--- components/spotify/TopTracks.tsx
+++ components/spotify/TopTracks.tsx
@@ -11,58 +11,34 @@ export default function TopTracks() {
 
     return !isLoading ? (
         <div className="flex flex-col gap-3">
-            {data
-                ? data.map((track: any, idx: number) => (
-                      <Link
-                          key={"tracks-" + idx}
-                          href={track.songUrl || "#"}
-                          className="font-normal no-underline"
-                      >
-                          <div className="flex h-[100px] items-center gap-3 rounded-md border-2 border-dashed border-gray-200 p-2 hover:bg-zinc-300 dark:border-gray-800 dark:hover:bg-zinc-700">
-                              <strong>#{idx + 1}</strong>
-                              <Image
-                                  className="rounded-md"
-                                  src={track.cover || NO_COVER}
-                                  alt={track.title}
-                                  height={80}
-                                  width={80}
-                                  style={{ width: 80, height: 80 }}
-                              />
-                              <div className="flex flex-col justify-center gap-1 text-[#202020] dark:text-gray-300">
-                                  <h5 className="m-0 line-clamp-1">
-                                      {track.title}
-                                  </h5>
-                                  <p className="m-0 line-clamp-1 text-sm dark:text-gray-300 md:text-base">
-                                      {track.artist}
-                                  </p>
-                              </div>
-                          </div>
-                      </Link>
-                  ))
-                : [...Array(5)].map((x, i) => (
-                      <Link
-                          key={x}
-                          href="#"
-                          className="font-normal no-underline"
-                      >
-                          <div className="flex h-[100px] items-center gap-3 rounded-md border-2 border-dashed border-gray-200 p-2 hover:bg-zinc-300 dark:border-gray-800 dark:hover:bg-zinc-700">
-                              <strong>#{i + 1}</strong>
-                              <Image
-                                  className="rounded-md"
-                                  src={NO_COVER}
-                                  alt="Not playing"
-                                  height={80}
-                                  width={80}
-                                  style={{ width: 80, height: 80 }}
-                              />
-                              <div className="flex flex-col justify-center gap-1 text-[#202020] dark:text-gray-300">
-                                  <h5 className="m-0">
-                                      Not listening to anything this month
-                                  </h5>
-                              </div>
-                          </div>
-                      </Link>
-                  ))}
+            {data &&
+                data.map((track: any, idx: number) => (
+                    <Link
+                        key={"tracks-" + idx}
+                        href={track.songUrl || "#"}
+                        className="font-normal no-underline"
+                    >
+                        <div className="flex h-[100px] items-center gap-3 rounded-md border-2 border-dashed border-gray-200 p-2 hover:bg-zinc-300 dark:border-gray-800 dark:hover:bg-zinc-700">
+                            <strong>#{idx + 1}</strong>
+                            <Image
+                                className="rounded-md"
+                                src={track.cover || NO_COVER}
+                                alt={track.title}
+                                height={80}
+                                width={80}
+                                style={{ width: 80, height: 80 }}
+                            />
+                            <div className="flex flex-col justify-center gap-1 text-[#202020] dark:text-gray-300">
+                                <h5 className="m-0 line-clamp-1">
+                                    {track.title}
+                                </h5>
+                                <p className="m-0 line-clamp-1 text-sm dark:text-gray-300 md:text-base">
+                                    {track.artist}
+                                </p>
+                            </div>
+                        </div>
+                    </Link>
+                ))}
         </div>
     ) : (
         <div className="flex flex-col gap-3">
