f8659891d8011ef39e0584c06b09bff1f616fe22
Khamid Arrazaq <saikonomia@gmail.com>
2023-12-31 16:09:25 +0700

    test

--- components/layout/navbar/index.tsx
+++ components/layout/navbar/index.tsx
@@ -23,23 +23,23 @@ export default function Navbar() {
             <div className="mx-auto flex max-w-[85%] items-center justify-between py-2">
                 <Link
                     href="/"
-                    className="rounded px-3 py-2 text-center font-doodle text-xl font-semibold transition duration-200 ease-in-out hover:bg-zinc-300 dark:hover:bg-zinc-700"
+                    className="rounded px-2.5 py-1.5 font-doodle text-xl font-semibold transition duration-200 ease-in-out hover:bg-zinc-300 dark:hover:bg-zinc-700"
                 >
                     Akhamr.tech
                 </Link>
                 <div className="flex">
-                    <div className="hidden md:flex">
+                    <div className="hidden space-x-3 md:flex">
                         {links.map((link, i) => (
                             <Link
                                 key={i}
                                 href={link.url}
-                                className="rounded px-5 py-3 text-center text-sm font-semibold transition duration-200 ease-in-out hover:bg-zinc-300 dark:hover:bg-zinc-700"
+                                className="rounded px-3 py-2.5 text-sm font-semibold transition duration-200 ease-in-out hover:bg-zinc-300 dark:hover:bg-zinc-700"
                             >
                                 {link.text}
                             </Link>
                         ))}
                     </div>
-                    <div className="flex items-center space-x-3 px-3">
+                    <div className="flex items-center space-x-3 pl-5">
                         <ThemeSwitcher />
                         <MobileMenu />
                     </div>
