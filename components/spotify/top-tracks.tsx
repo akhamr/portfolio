3462ba799fbc2759b367705e5bc37c4e89447c58
Khamid Arrazaq <saikonomia@gmail.com>
2024-02-05 14:23:42 +0700

    mantap

--- components/spotify/top-tracks.tsx
+++ components/spotify/top-tracks.tsx
@@ -9,13 +9,13 @@ export default function TopTracks() {
     const fetcher = (url: string) => fetch(url).then((r) => r.json());
     const { data, isLoading } = useSWR("/api/top-tracks", fetcher);
 
-    return !isLoading ? (
+    return (
         <div className="flex flex-col gap-3">
-            {data
-                ? data.map((track: any, idx: number) => (
+            {!isLoading
+                ? data?.map((track: any, idx: number) => (
                       <Link
                           target="_blank"
-                          key={"tracks-" + idx}
+                          key={idx}
                           href={track.songUrl || "#"}
                           className="font-normal no-underline"
                       >
@@ -40,25 +40,21 @@ export default function TopTracks() {
                           </div>
                       </Link>
                   ))
-                : null}
-        </div>
-    ) : (
-        <div className="flex flex-col gap-3">
-            {[...Array(5)].map((x, i) => (
-                <div
-                    key={x}
-                    className="h-[100px] rounded-md border-2 border-dashed border-gray-200 p-2 dark:border-gray-800"
-                >
-                    <div className="flex animate-pulse items-center gap-3">
-                        <strong>#{i + 1}</strong>
-                        <div className="h-[80px] w-[80px] rounded-md bg-zinc-300 dark:bg-zinc-700"></div>
-                        <div className="flex flex-col justify-center gap-3">
-                            <div className="h-6 w-40 rounded bg-zinc-300 md:w-64 dark:bg-zinc-700"></div>
-                            <div className="h-5 w-28 rounded bg-zinc-300 md:w-48 dark:bg-zinc-700"></div>
-                        </div>
-                    </div>
-                </div>
-            ))}
+                : [...Array(5)].map((_, i) => (
+                      <div
+                          key={i}
+                          className="h-[100px] rounded-md border-2 border-dashed border-gray-200 p-2 dark:border-gray-800"
+                      >
+                          <div className="flex animate-pulse items-center gap-3">
+                              <strong>#{i + 1}</strong>
+                              <div className="h-[80px] w-[80px] rounded-md bg-zinc-300 dark:bg-zinc-700"></div>
+                              <div className="flex flex-col justify-center gap-3">
+                                  <div className="h-6 w-40 rounded bg-zinc-300 md:w-64 dark:bg-zinc-700"></div>
+                                  <div className="h-5 w-28 rounded bg-zinc-300 md:w-48 dark:bg-zinc-700"></div>
+                              </div>
+                          </div>
+                      </div>
+                  ))}
         </div>
     );
 }
