eb2c5028345ab8c28c2a51fcefde99852f9b07de
Khamid Arrazaq <saikonomia@gmail.com>
2024-02-05 12:47:38 +0700

    tes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,10 @@
 module.exports = {
     darkMode: "class",
     content: [
-        "./data/**/*.{js,ts,jsx,tsx,mdx}",
-        "./components/**/*.{js,ts,jsx,tsx,mdx}",
         "./app/**/*.{js,ts,jsx,tsx,mdx}",
-        "./hooks/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./data/**/*.{js,ts,jsx,tsx,mdx}",
+        "./lib/**/*.{js,ts,jsx,tsx,mdx}",
     ],
     theme: {
         extend: {
