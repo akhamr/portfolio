89e88f47708c9c1920c7dc2db34b2106ca6582cf
Khamid Arrazaq <saikonomia@gmail.com>
2024-05-26 14:32:51 +0700

    fix: robots.txt

--- app/robots.ts
+++ app/robots.ts
@@ -5,6 +5,7 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: "*",
+      allow: "/",
     },
     sitemap: `${baseUrl}/sitemap.xml`,
     host: baseUrl,
